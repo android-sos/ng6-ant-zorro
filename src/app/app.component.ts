dfcefbdae198a0a0e7495e81873a8d59b591e931
samuel <samuel.ospina36@gmail.com>
2018-10-09 23:04:36 -0500

    Reactive form Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[FormBuilder]
 })
-export class AppComponent {
-  title = 'ng6-ant-zorro';
+export class AppComponent implements OnInit {
+  validateForm: FormGroup;
+
+  submitForm(): void {
+    console.log(this.validateForm.value);
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      datePicker     : [ null ],
+      datePickerTime : [ null ],
+      monthPicker    : [ null ],
+      rangePicker    : [ [] ],
+      rangePickerTime: [ [] ],
+      timePicker     : [ null ]
+    });
+  }
 }
